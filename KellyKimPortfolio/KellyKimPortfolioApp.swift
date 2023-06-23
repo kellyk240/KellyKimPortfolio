7f331d7ee1fc0d29cb77aa7ac78107f9333caaf7
Kelly Kim <kellykim240@gmail.com>
2023-06-23 15:08:46 -0400

    Initial Commit

---
+++ KellyKimPortfolio/KellyKimPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  KellyKimPortfolioApp.swift
+//  KellyKimPortfolio
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct KellyKimPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
